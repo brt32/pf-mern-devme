ea79565ec85dc793b813472b42e0fd4dd8d467cf
Bartek <bartlomiej.proczkowski@gmail.com>
2021-07-17 23:16:14 +0200

    Backend Done!

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer text-center py-3">
+        Copyright &copy; Proczkowski DevMe
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
+// // F:\Administracja SIECI - NAUKA\09. PROGRAMMING\01. Front-End\Udemy - MERN Stack Front To Back Full Stack React, Redux & Node.js [BT]
+
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extented: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5002;
 
